882cb5ae96d9b301dd88a79d6181e50d1d3b1ac8
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2022-03-01 23:50:30 -0300

    Atualizando

--- index.php
+++ index.php
@@ -1,31 +1,23 @@
 <?php
 
+use App\Class\UploadFoto;
+use App\Class\UploadFile;
+
+require "vendor/autoload.php";
 require "vendor/autoload.php";
 
-use App\classes\UploadFotos;
-
-$upload = new UploadFotos();
-
-$upload->file('foto.png');
-$upload->extension();
-$upload->rename();
+$upload = new UploadFoto('Foto.png');
 echo $upload->upload();
 
-
-
-
-//$atividadesPessoa = new AtividadesPessoa();
-//echo $atividadesPessoa->pular();
-//echo $atividadesPessoa->andar();
-
+$upload = new UploadFile('Foto.zip');
+echo $upload->upload();
 
 //$pessoa = new Pessoa();
-//$pessoa->idade = 26;
+//$pessoa->idade = 35 ;
 //$pessoa->nome = "Allan Rodrigues";
-//$pessoa->email = "Allan@php.com.br";
+//$pessoa->email = "allan@php.com.br " ;
 //echo $pessoa->dados();
-//
-//$pessoa->idade = 35;
-//$pessoa->nome = "Rodrigues Machado";
-//$pessoa->email = "Allan@php.com.br";
-//echo $pessoa->dados();
+
+//$atividadesPessoa = new AtividadePessoa();
+//$atividadesPessoa->pular();
+//$atividadesPessoa->andar();
